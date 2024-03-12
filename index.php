16d6fa0f00e3d5ef0de873d109556388f8997b7b
Vincentcolantonio <160608722+Vincentcolantonio@users.noreply.github.com>
2024-03-12 13:32:18 +0100

    Commit generico per caricare tutti i file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Libreria</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-light-subtle">
     <div class="container w-50">
         <div class="row">
             <div class="col gy-5">
@@ -36,7 +36,11 @@
                 </table>
             </div>
         </div>
-        
+        <div class="row">
+            <div class="col">
+                <a href="form.html"><button class="btn btn-primary">Aggiungi File</button></a>
+            </div>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- upload.php
+++ upload.php
@@ -3,6 +3,15 @@ $file = $_FILES["pdf"];
 if ($file["type"] === "application/pdf") {
     //Sanificazione del filename (giusto per sicureza)
     $sanified = basename($file["name"]);
+    $directory = opendir("./uploadedFiles");
+    $file = readdir($directory);
+    $conta = 0;
+    while ($file) {
+        if ($sanified === $file) {
+            $sanified = $sanified."(".++$conta.")";
+        }
+        $file = readdir($directory);
+    }
     move_uploaded_file($file["tmp_name"], "./uploadedFiles/".$sanified);
 }
 header("Location: index.php");
